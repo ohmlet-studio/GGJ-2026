9a6fc6a380788ccf483b1967cb9fd6aa247c89d7
RubenGres <ruben.gres@proton.me>
2026-01-31 23:10:09 +0100

    scripts on animation

--- Scenes/DinnerTable/hint_anim.gd
+++ Scenes/DinnerTable/hint_anim.gd
@@ -6,3 +6,4 @@ func _ready() -> void:
 func _update_animation_speed(tempo_speed_ms: float):
 	print("_update_animation_speed ", tempo_speed_ms)
 	self.speed_scale = 1000.0 / tempo_speed_ms
+	self.set_frame_and_progress(0, 0.0)
