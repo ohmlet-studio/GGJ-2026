a8df6fbba23a77b8040fbe3fdfcf3f8d7e26c941
Ollie Chapman <57490535+OllieeeChap@users.noreply.github.com>
2026-01-31 17:26:39 +0100

    Random sfx in audio controller

--- Scenes/AudioController/audio_controller.gd
+++ Scenes/AudioController/audio_controller.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+## SFX arrays
+@onready var _sad 	= [$Sad_sfx_1, $Sad_sfx_2, $Sad_sfx_3, $Sad_sfx_4, $Sad_sfx_5]
+@onready var _happy	= [$Happy_sfx_1, $Happy_sfx_2, $Happy_sfx_3, $Happy_sfx_4, $Happy_sfx_5, $Happy_sfx_6]
+@onready var _angry	= [$Angry_sfx_1, $Angry_sfx_2, $Angry_sfx_3, $Angry_sfx_4, $Angry_sfx_5, $Angry_sfx_6]
+@onready var _surprised = [$Surprised_sfx_1, $Surprised_sfx_2, $Surprised_sfx_3, $Surprised_sfx_4, $Surprised_sfx_5]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,10 +17,10 @@ func play_feelings(direction):
 	# with directions
 	match direction:
 		SimonDirections.UP:
-			$Happy_sfx.play()
+			_happy.pick_random().play()
 		SimonDirections.DOWN:
-			$Sad_sfx.play()
+			_sad.pick_random().play()
 		SimonDirections.LEFT:
-			$Angry_sfx.play()
+			_angry.pick_random().play()
 		SimonDirections.RIGHT:
-			$Surprised_sfx.play()
+			_surprised.pick_random().play()
